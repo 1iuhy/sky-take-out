3dbd647ee8f21b021c8cdb28221b967547623b25
1iuhy <1iuhy20050705@gmail.com>
2025-05-11 16:49:02 +0800

    start Or Stop Set Meal
    起售停售套餐

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -87,4 +87,18 @@ public class SetmealController {
         setmealService.update(setmealDTO);
         return Result.success();
     }
+
+    /**
+     * 起售停售套餐
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("起售停售套餐")
+    public Result startOrStop(@PathVariable Integer status, Long id){
+        log.info("起售停售套餐");
+        setmealService.startOrStop(status,id);
+        return Result.success();
+    }
 }
